a3df6ac9a4d45647562a18dbdd29f94e2f49c15c
khoaphamtan2311 <tankhoapham2311@gmail.com>
2023-06-20 13:08:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import "Your code here";
+import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
 
 function App() {
-  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+  const {
+    time,
+    startTimer,
+    stopTimer,
+    resetTimer,
+    active,
+    recordedTimes,
+    handleRecord,
+  } = useTimer(0);
 
   return (
     <div className="App container">
       <h1>Coder Timer</h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{"Your code here"}</p>
+          <p>{formatTime(time)}</p>
         </div>
         <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={stopTimer}>
             Stop
           </button>
-          <button
-            className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
-          >
+          <button className="button" ref={active} onClick={startTimer}>
             Start
           </button>
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={resetTimer}>
             Reset
           </button>
+          <button className="button" onClick={handleRecord}>
+            Record
+          </button>
+        </div>
+        <div className="record">
+          {recordedTimes.map((recordedTime, index) => (
+            <h3 key={index}>
+              [{index + 1}] {recordedTime}
+            </h3>
+          ))}
         </div>
       </div>
     </div>
--- src/useTimer.js
+++ src/useTimer.js
@@ -1,24 +1,50 @@
 import { useState, useRef } from "react";
+import { formatTime } from "./formatTime";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+  const [time, setTime] = useState(ini);
+  const [recordedTimes, setRecordedTimes] = useState([]);
 
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const isStart = useRef(false);
+  const active = useRef();
+  const refInterval = useRef(0);
 
   const startTimer = () => {
     "Your code here";
     active.current.disabled = true;
+    isStart.current = true;
+    refInterval.current = setInterval(() => {
+      if (isStart.current) {
+        setTime((time) => time + 1);
+      }
+    }, 1000);
   };
   const stopTimer = () => {
-    "Your code here";
-  };
-  const resetTimer = () => {
-    "Your code here";
+    isStart.current = false;
+
+    clearInterval(refInterval.current);
     active.current.disabled = false;
   };
+  const resetTimer = () => {
+    setTime(ini);
+    setRecordedTimes([]);
+    clearInterval(refInterval.current);
+    active.current.disabled = false;
+    isStart.current = false;
+  };
 
-  return { time, startTimer, stopTimer, resetTimer, active };
+  const handleRecord = () => {
+    setRecordedTimes([...recordedTimes, formatTime(time)]);
+  };
+
+  return {
+    time,
+    startTimer,
+    stopTimer,
+    resetTimer,
+    active,
+    recordedTimes,
+    handleRecord,
+  };
 };
 export default useTimer;
